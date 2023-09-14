8a49cbb2d870097c3de30810059f61a85af9cfd9
Almazbekuulu <almazbekuulutalant20@gmail.com>
2023-09-14 21:52:08 +0600

    home work

--- week1/lesson1.py
+++ week1/lesson1.py
@@ -1,1 +1,18 @@
+#Задача номер 1 множества
 
+list1 = set(input().split())
+list2 = set(input().split())
+print(set.intersection(list1,list2))
+
+#Задача номер 2 множества
+list3 = set(input().split())
+list4 = set(input().split())
+print(set.difference(list3,list4))
+
+#Задача номер 3 множества
+list5 = set(input().split())
+print(list5)
+
+#Задача номер 4 множества
+list6 = set(input().split(','))
+print(len(list6))
